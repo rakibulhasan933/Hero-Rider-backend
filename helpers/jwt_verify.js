44eaddc74d4735c90f3e033d0cf99140c4bfb317
RAKIBUL <rakibulmd933@gmail.com>
2023-03-22 14:53:13 +0600

    [ADD] admin panel

--- helpers/jwt_verify.js
+++ helpers/jwt_verify.js
@@ -1,12 +1,11 @@
 const jwt = require('jsonwebtoken');
 
-const JWTVerify = (req, res, next) => {
+const JWTVerify = async (req, res, next) => {
 	const { authorization } = req.headers;
 	try {
-		const token = authorization.split(' ')[1];
+		const token = await authorization.split(' ')[1];
 		const decoded = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
 		const { email } = decoded;
-		console.log(decoded);
 		req.email = email;
 		next();
 	} catch (error) {
